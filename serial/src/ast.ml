0b0716fad17411d127381c1e1c95c0137121247c
Yagotzirck <yagotzirck@gmail.com>
2021-10-22 00:11:38 +0200

    Implemented the program store (sigma)

--- serial/src/ast.ml
+++ serial/src/ast.ml
@@ -10,6 +10,7 @@ type int_expr =
   | Add of int_expr * int_expr
   | Sub of int_expr * int_expr;;
 
+
 type bool_expr =
   | Ebool of bool
   | Not of bool_expr
@@ -18,7 +19,9 @@ type bool_expr =
   | And of bool_expr * bool_expr;;
 
 
-type program = Program of stmt list (* Should add state and enhanced state as well *)
+type program =
+  | Program_empty
+  | Program of stmt * program
 
 and
 
