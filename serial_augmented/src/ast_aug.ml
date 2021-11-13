560ee05cb16cfe78218325dbc891366d558b4096
Yagotzirck <yagotzirck@gmail.com>
2021-11-13 13:10:25 +0100

    Added/fixed comments

--- serial_augmented/src/ast_aug.ml
+++ serial_augmented/src/ast_aug.ml
@@ -11,6 +11,14 @@ type program_aug =
 
 and
 
+(* Statements must be augmented as well with the following additions:
+**
+** - Program_start and Program_end, in order to set boundaries for the whole program;
+**
+** - If_start and If_end, in order to set boundaries for programs inside the "then" and "else" branches.
+** 
+** - While_start and While_end, in order to set boundaries for programs constituting While blocks.
+*)
 stmt_aug =
   | Program_start
   | Program_end
@@ -22,10 +30,17 @@ stmt_aug =
   | Csub of int_expr * int_expr (* Same as above *)
 
   | Ifthenelse of bool_expr * program_aug * program_aug
-  (* If_start: (prg_prev), where prg_prev the program having the outer While containing this program block as the current statement *)
-  | If_start of program_aug       
-  (* If_end: (prg_next, if_b_eval), where prg_next the program having the statement that follows the outer While containing this program block as the current statement *)
-  | If_end of program_aug * bool  
+
+  (* If_start: (prg_prev), where prg_prev is the program having the outer If containing this program block as the current statement *)
+  | If_start of program_aug     
+
+  (* If_end: (prg_next, if_b_eval), where:
+  **  
+  **  - prg_next is the program having the statement that follows the outer While containing this program block as the current statement;
+  **  
+  **  - if_b_eval is the bool value resulting from the evaluation of the containing Ifthenelse's bool expression.
+  *)
+  | If_end of program_aug * bool
 
   | While of bool_expr * program_aug
   | While_start of program_aug              (* (while_in_outer_prg *)
