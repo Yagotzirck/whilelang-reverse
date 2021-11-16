61ced59b5ebaaa3213e9c15b80f9237969402ecc
Yagotzirck <yagotzirck@gmail.com>
2021-11-16 08:28:24 +0100

    Added a description of the zipper structure used for program_aug

--- serial_augmented/src/ast_aug.ml
+++ serial_augmented/src/ast_aug.ml
@@ -5,6 +5,43 @@ open Ast
 (** In the augmented version we must allow forwards as well as backwards execution,
     therefore we must implement a doubly-linked list (or rather, its functional
     equivalent: a zipper) in order to allow movement in both directions.
+
+    In order to explain how a zipper works, let's start from this simple integer list:
+
+    [[1; 2; 3; 4; 5; 6]]
+
+    Its zipper representation would consist in a tuple containing:
+      - A list containing the items to the left of the current element ([prev_items]);
+      - The current item ([curr_item]);
+      - A list containing the elements to the right of the current element ([next_items]).
+
+    Therefore, the previous list converted to a zipper would be:
+
+    [( [], 1, [2; 3; 4; 5; 6] )]
+
+    A [next_item] operation
+      - moves [curr_item] to the head of [prev_items] list;
+      - moves the head of [next_items] to [curr_item].
+
+    Applying [next_item] to the previous zipper yields
+
+    [( 1 :: [], 2, [3; 4; 5; 6] )]
+
+    If we apply [next_item] two more times, we get
+
+    [( [3; 2; 1]), 4, [5; 6] )]
+
+    A [prev_item] operation
+      - moves [curr_item] to the head of [next_items] list;
+      - moves the head of [prev_items] to [curr_item].
+    Applying [prev_item] to the last zipper we obtained above yields
+
+    [( [2; 1]), 3, [4; 5; 6] )]
+
+    It should be clear at this point that the zipper is the ideal structure that achieves a
+    doubly linked list-like behavior using the functional programming paradigm, allowing us to
+    move in both directions starting from a given list of items (in our case, a list of
+    statements/instructions).
 *)
 type program_aug =
   | Program_aug of stmt_aug list * stmt_aug * stmt_aug list
@@ -22,7 +59,7 @@ and stmt_aug =
 
   (* Cop (constructive assignments on variables: "+=" and "-=") *)
   | Cadd of int_expr * int_expr (** Constructive assignment ("+="); must check about 1st int_expr being a Val *)
-  | Csub of int_expr * int_expr (** Constructive assignment ("+="); must check about 1st int_expr being a Val *)
+  | Csub of int_expr * int_expr (** Constructive assignment ("-="); must check about 1st int_expr being a Val *)
 
   | Ifthenelse of bool_expr * program_aug * program_aug
 
