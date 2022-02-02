15d5a84a01612367d59d6fc5831722d1b2f53607
Yagotzirck <yagotzirck@gmail.com>
2022-02-02 17:25:27 +0100

    Added child_branch field to type thread, added source code stack operations (wrappers for Program's push/pop/top operations)

--- parallel/src/lib/thread.ml
+++ parallel/src/lib/thread.ml
@@ -2,6 +2,12 @@
 
 open Ast_ann
 
+(** type used in the [thread] tuple, to discern whether the thread contains a program
+    contained in the first (Left) or in the second (Right) element of the [Par] statement
+    of the parent thread that created it.
+*)
+type child_branch = Left | Right | Root;;
+
 (** A thread is a tuple containing the following fields:
 
   + [prg]:            The annotated program associated to the thread;
@@ -10,53 +16,68 @@ open Ast_ann
                       as the thread terminates;
   + [ptid]:           An integer value that identifies the parent thread's ID
                       (that is, the [tid] of the thread that generated this thread);
+  + [branch]:         A variable of type [child_branch], indicating whether the thread is
+                      a [Left] child or a [Right] child;
   + [num_chld_done]:  An integer value that keeps track of how many threads generated
                       by the current thread are done executing their annotated program [prg].
                       This is useful because as soon as all of the child threads are done (2, in our case),
                       the parent thread must be moved from [waiting_threads] to [running_threads]
                       in the [State] module.
 *)
-type thread = Thread of program_ann * int * int * int;;
+type thread = Thread of program_ann * int * int * child_branch * int;;
 
 (** Creates and returns a new thread, initializing it with the given parameters
-    (annotated program, thread id and the id of the thread who requested the creation)
+    (annotated program, thread id, id of the thread who requested the creation, and branch)
     and setting the number of child threads which finished their execution to 0 (for obvious reasons).
 *)
-let create prg tid ptid =
-  Thread (prg, tid, ptid, 0);;
+let create prg tid ptid branch =
+  Thread (prg, tid, ptid, branch, 0);;
 
 (** This is a wrapper which applies {!val:Program.prev_stmt} to the program contained inside the specified
     thread parameter.
 *)
 let prev_stmt = function
-  | Thread (prg, tid, ptid, num_chld_done) -> Thread (Program.prev_stmt prg, tid, ptid, num_chld_done);;
+  | Thread (prg, tid, ptid, branch, num_chld_done) -> Thread (Program.prev_stmt prg, tid, ptid, branch, num_chld_done);;
 
 (** This is a wrapper which applies {!val:Program.get_prev_stmt} to the program contained inside the specified
     thread parameter.
 *)
 let get_prev_stmt = function
-  | Thread (prg, _, _, _) -> Program.get_prev_stmt prg;;
+  | Thread (prg, _, _, _, _) -> Program.get_prev_stmt prg;;
 
 
 (** This is a wrapper which applies {!val:Program.get_curr_stmt} to the program contained inside the specified
     thread parameter.
 *)
 let get_curr_stmt = function
-  | Thread (prg, _, _, _) -> Program.get_curr_stmt prg;;
+  | Thread (prg, _, _, _, _) -> Program.get_curr_stmt prg;;
 
 (** This is a wrapper which applies {!val:Program.next_stmt} to the program contained inside the specified
     thread parameter.
 *)
 let next_stmt = function
-  | Thread (prg, tid, ptid, num_chld_done) -> Thread (Program.next_stmt prg, tid, ptid, num_chld_done);;
+  | Thread (prg, tid, ptid, branch, num_chld_done) -> Thread (Program.next_stmt prg, tid, ptid, branch, num_chld_done);;
 
 (** This is a wrapper which applies {!val:Program.push_stmt_counter} to the program contained inside the specified
     thread parameter.
 *)
 let push_stmt_counter stmt_counter = function
-  | Thread (prg, tid, ptid, num_chld_done) -> Thread (Program.push_stmt_counter stmt_counter prg, tid, ptid, num_chld_done);;
+  | Thread (prg, tid, ptid, branch, num_chld_done) -> Thread (Program.push_stmt_counter stmt_counter prg, tid, ptid, branch, num_chld_done);;
+
+(** This is a wrapper which applies {!val:Program.top_stmt_counter} to the program contained inside the specified
+    thread parameter.
+*)
+let top_prev_stmt_counter = function
+  | Thread (prg, _, _, _, _) -> Program.top_prev_stmt_counter prg;;
+
+
+(** This is a wrapper which applies {!val:Program.pop_prev_stmt_counter} to the program contained inside the specified
+    thread parameter.
+*)
+let pop_prev_stmt_counter = function
+  | Thread (prg, _, _, _, _) -> Program.pop_prev_stmt_counter prg;;
 
 
 (** Given a thread, returns its ID. *)
 let get_tid = function
-  | Thread (_, tid, _, _) -> tid;;
+  | Thread (_, tid, _, _, _) -> tid;;
